56cb016441509971f352873e6bf02dfc64fa5e19
Tousif Tamboli <tousiftamboli3@gmail.com>
2024-10-27 22:46:08 +0530

    product page doing 2:54:59

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 const Product = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { productId } = useParams();
+  const { products } = useContext(ShopContext);
+  const [productData, setProductData] = useState(null);
+  const [image, setImage] = useState("");
 
-export default Product
+  const fetchProductData = () => {
+    const product = products.find((item) => item._id === productId);
+    if (product) {
+      setProductData(product);
+      setImage(item.image[0])
+    }
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products]);
+
+  return productData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+    {/* Product Data */}
+    <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+
+    </div>
+
+    </div>
+    
+  ) : <div className="opacity-0"></div>
+};
+
+export default Product;
