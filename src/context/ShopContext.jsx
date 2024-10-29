44b0e383ab8527b15ef2014578d06b74dc1016df
Tousif Tamboli <tousiftamboli3@gmail.com>
2024-10-29 15:37:38 +0530

    done till 4:03:59 / 5:10:52

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -54,12 +54,19 @@ const ShopContextProvider = (props) =>{
     }
 
  
+    const updateQuantity = async (itemId,size,quantity) =>{
+        let cartData = structuredClone(cartItems);
+
+        cartData[itemId][size] = quantity;
+
+        setCartItems(cartData);
+    }
 
     const value = {
         products, currency, delivery_fee,
         search, setSearch,showSearch,setShowSearch,
         cartItems, addToCart,
-        getCartCount
+        getCartCount, updateQuantity
 
     }
 
