cfbdd0725e8f0ece49318f7e8539b5b0340116f4
Tousif Tamboli <tousiftamboli3@gmail.com>
2024-10-30 18:14:02 +0530

    login and register endpoint api working properly

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     
-    await mongoose.connect(`mongodb+srv://tousiftamboli3:7666839558@cluster0.n1nrh.mongodb.net/mydb`)
 
     console.log("DB is connected")
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/users`)
 
 };
 
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,6 +9,33 @@ const createToken = (id) =>{
 
 //route for userlogin
 const loginUser = async (req, res) =>{
+    try {
+
+        const {email, password} = req.body;
+
+        const user = await userModel.findOne({email});
+
+        if(!user){
+            return res.json({success:false, message:"User does not exists"})
+
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(isMatch){
+            const token = createToken(user._id)
+            res.json({success:true,token})
+        }
+        else
+        {
+            res.json({success:false, message: "Invalid"})
+        }
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message})
+        
+    }
 
 
 }
