07b29b32adc5d6c26c31781098d2257808f6ab27
Tousif Tamboli <tousiftamboli3@gmail.com>
2024-11-01 23:05:34 +0530

    6:56:00 / 13:32:41

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,6 @@
+import { v2 as cloudinary } from "cloudinary";
+import productModel from "../models/productModel";
+
 //for adding product
 const addProduct = async (req, res) => {
   try {
@@ -11,23 +14,42 @@ const addProduct = async (req, res) => {
       bestseller,
     } = req.body;
 
-    const image1 = req.files.image1[0];
-    const image2 = req.files.image2[0];
-    const image3 = req.files.image3[0];
-    const image4 = req.files.image4[0];
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
 
-    console.log(
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item != undefined
+    );
+
+    let imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      })
+    );
+
+    const productData = {
       name,
       description,
-      price,
       category,
+      price: Number(price),
       subCategory,
-      sizes,
-      bestseller
-    );
-    console.log(image1, image2, image3, image4);
+      bestseller: bestseller === "true" ? true : false,
+      sizes: JSON.parse(sizes),
+      image: imagesUrl,
+      date: Date.now(),
+    };
 
-    res.json({});
+    console.log(productData);
+
+    const product = new productModel(productData);
+    await product.save();
+
+    res.json({ success: true, message: "Product added" });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
